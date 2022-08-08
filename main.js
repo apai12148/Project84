9fb2fdc2fc07d0bafb36ee06aa73ccfc61c546eb
apai12148 <97473664+apai12148@users.noreply.github.com>
2022-08-08 09:36:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,111 +1,98 @@
-//Create a reference for canvas 
+// Create a reference for the canvas
 canvas = document.getElementById("myCanvas");
-ctx = canvas.getContext("2d")
-//Give specific height and width to the car image
-greeencar_width = 75;
-greencar_height = 100;
-background_img = "parkingLot.jpg";
-greencar_image = "car2.png";
+ctx = canvas.getContext("2d");
 
-//Set initial position for a car image.
-greencar_x = 5;
-greencar_y = 225;
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
 function add() {
-	//upload car, and background images on the canvas.
-	background_imgTag = new Image();
-	background_imgTag.onload = uploadBackground;
-	background_imgTag.src = background_img;
-
-	greencar_imgTag = new Image();
-	greencar_imgTag.onload = uploadgreencar;
-	greencar_img.src = greencar_img;
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
-function uploadBackground() {
-	//Define function ‘uploadBackground’
-	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-function uploadgreencar() {
-	//Define function ‘uploadgreencar’.
-	ctx.drawImage(greeencar_imgTag, greencar_x,greencar_y, greencar_width, greencar_height)
-	
-}
-
-
+//Write a code to grab the key-pressed event
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-		if(keyPressed == '38')
-		{
-			up();
-			console.log("up");
-		}
 	
-		if(keyPressed == '40')
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
 		{
-			down();
-			console.log("down");
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+			console.log("alphabet key")
+
 		}
-		
-		if(keyPressed == '37')
-		{
-			left();
-			console.log("left");
+		else if((keyPressed >=48 && keyPressed<=57)){
+			numberkey();
+			document.getElementById("d1").innerHTML="You pressed Number Key";
+			console.log("number key")
 		}
+		else if((keyPressed >=37 && keyPressed<=40)){
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed Arrow Key";
+			console.log("arrow key")
+		}
+		else if((keyPressed >=17)){
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed Special Key";
+			console.log("special key")
+		}
+		else if((keyPressed >=18)){
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed Special Key";
+			console.log("special key")
+		}
+		else if((keyPressed >=27)){
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed Special Key";
+			console.log("special key")
+		}
+		//write a code to check the type of key pressed
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+		console.log("other key")
+	}
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message. 
+	img_image = "Alpkey.png";
+	add();
+}
+function numberkey()
+{
+	img_image = "numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image = "Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image = "spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
 	
-		if(keyPressed == '39')
-		{
-			right();
-			console.log("right");
-		}
-		
-		
-}
-
-function up()
-{
-	//Define function to move the car upward
-	if(greencar_y >=0){
-		greencar_y = greencar_y - 10;
-		console.log("When up arrow is presed,  x = " + greencar_x + " y = " +greencar_y);
-		uploadBackground();
-		upload_greencar();
-	}
-}
-
-function down()
-{
-	//Define function to move the car downward
-	if(greencar_y >=300){
-		greencar_y = greencar_y + 10;
-		console.log("When up arrow is presed,  x = " + greencar_x + " y = " +greencar_y);
-		uploadBackground();
-		upload_greencar();
-	}
-}
-
-function left()
-{
-	//Define function to move the car left side
-	if(greencar_x >=0){
-		greencar_x = greencar_x - 10;
-		console.log("When up arrow is presed,  x = " + greencar_x + " y = " +greencar_y);
-		uploadBackground();
-		upload_greencar();
-	}
-}
-
-function right()
-{
-	//Define function to move the car right side
-	if(greencar_x >=700){
-		greencar_x = greencar_x + 10;
-		console.log("When up arrow is presed,  x = " + greencar_x + " y = " +greencar_y);
-		uploadBackground();
-		upload_greencar();
-	}
-}
